f39eb964539c081800bb1beb255ec7e8b20e902f
Guilherme Figueira <gr.figueira@campus.fct.unl.pt>
2023-11-13 22:58:31 +0000

    Added post buttons and name input

--- lib/postpage.dart
+++ lib/postpage.dart
@@ -10,6 +10,53 @@ class PostPage extends StatefulWidget {
 class _PostPageState extends State<PostPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text('Create Recipe',
+              style: TextStyle(color: Colors.black))),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 255, 255, 255),
+                    onPrimary: Colors.black,
+                  ),
+                  child: const Text('Post'),
+                ),
+              ],
+            ),
+          ),
+          Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(12)),
+              child: Row(
+                children: [
+                  const Text('Name'),
+                  const SizedBox(width: 10),
+                  Expanded(
+                      child: TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Enter the recipe name',
+                      border: InputBorder.none,
+                    ),
+                  ))
+                ],
+              ))
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Text('Post'),
+      ),
+    );
   }
 }
