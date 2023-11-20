2107bcd7e5e1c964309cdbc5184a1ba838028c98
André Tomás Ribeiro <at.ribeiro@campus.fct.unl.pt>
2023-11-20 18:29:20 +0000

    searchpage completed

--- lib/searchpage.dart
+++ lib/searchpage.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:feast/filters.dart';
+import 'package:feast/recipe.dart';
+import 'package:feast/recipePage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -11,6 +16,51 @@ class _SearchPageState extends State<SearchPage> {
   bool _useMyDiet = false;
   bool _digitalFridge = false;
 
+  List<Map<String, dynamic>> recipes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadRecipeFiles();
+  }
+
+  Future<void> loadRecipeFiles() async {
+    try {
+      String jsonFileNames =
+          await rootBundle.loadString('assets/recipes/recipe_list.json');
+      List<dynamic> dynamicFileNames = json.decode(jsonFileNames);
+
+      List<String> fileNames = [];
+
+      for (var item in dynamicFileNames) {
+        if (item is String) {
+          fileNames.add(item);
+        } else {
+          print('Invalid data structure for file name: $item');
+        }
+      }
+
+      for (String fileName in fileNames) {
+        String assetPath = 'assets/recipes/$fileName';
+        ByteData data = await rootBundle.load(assetPath);
+        String jsonString = utf8.decode(data.buffer.asUint8List());
+        Map<String, dynamic> recipeData = json.decode(jsonString);
+
+        // Ensure that the loaded data has the correct structure
+        if (recipeData is Map<String, dynamic>) {
+          recipes.add(recipeData);
+        } else {
+          print('Invalid data structure for recipe: $recipeData');
+        }
+      }
+
+      // Update the UI
+      setState(() {});
+    } catch (e) {
+      print('Error loading recipe files: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +68,7 @@ class _SearchPageState extends State<SearchPage> {
         title: const Text('Recipe Search'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch, // Ensure full width
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
@@ -88,7 +139,107 @@ class _SearchPageState extends State<SearchPage> {
               ],
             ),
           ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: recipes.length,
+              itemBuilder: (context, index) {
+                final recipe = recipes[index];
+                // Check if the recipe matches the search query
+                bool matchesSearch = recipe['name']
+                        ?.toLowerCase()
+                        .contains(_searchQuery.toLowerCase()) ??
+                    false;
+
+                // TODO: Check if other filtering criteria are met (e.g., My Diet, Digital Fridge)
+
+                // Only display the recipe card if it matches the search query and other criteria
+                if (matchesSearch) {
+                  return buildRecipeCard(recipe);
+                } else {
+                  // Return an empty container if the recipe should be excluded
+                  return Container();
+                }
+              },
+            ),
+          ),
         ],
+      ),
+    );
+  }
+
+  Widget buildRecipeCard(Map<String, dynamic> recipe) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => RecipePage(recipe: Recipe.fromJson(recipe)),
+          ),
+        );
+      },
+      child: Center(
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Image on the left side
+              Container(
+                width: 100, // Adjust the width as needed
+                height: 90, // Adjust the height as needed
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  image: DecorationImage(
+                    image: AssetImage(recipe['photo'] ?? ''),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 15),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      recipe['name'] ?? '',
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(height: 2),
+                    Text(
+                      'Duration: ${recipe['time'] ?? ''} minutes',
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 13,
+                      ),
+                    ),
+                    Text(
+                      'Tags: ${recipe['tags'] ?? ''}',
+                      style: const TextStyle(
+                        fontSize: 13,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
